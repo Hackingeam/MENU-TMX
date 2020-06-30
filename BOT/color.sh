05b966a8ab1aa1705c112f44b52d9f3c70097921
Hackingeam <65829735+Hackingeam@users.noreply.github.com>
2020-06-30 14:09:14 -0400

    Update color.sh

--- BOT/color.sh
+++ BOT/color.sh
@@ -1,1 +1,15 @@
+# Colores Bash
+#
+export rosa="\033[38;5;207m"
+export rojo='\033[1;31m'
+export verde='\033[32m'
+export amarillo='\033[1;33m'
+export azul='\033[1;34m'
+export morado='\033[1;35m'
+export blanco='\033[1;37m'
+export cyan='\033[36m'
+export magenta='\033[1;35m'
+export ter='\033[0m'
+export gris='\033[30m'
+export EAM='\033[1;0m\033[33m'
 
